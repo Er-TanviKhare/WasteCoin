e070e52f1a5f89fca595632fe2f4ff922f7f3a4d
Tanvi <er.tanvikhare@gmail.com>
2026-01-22 10:59:57 +0530

    Move Firebase config to env and ignore env files

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -7,17 +7,18 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOHQaAhpXqJK-BjjFnuijnJAD190OQtj8",
-  authDomain: "wastecoin-378f4.firebaseapp.com",
-  projectId: "wastecoin-378f4",
-  storageBucket: "wastecoin-378f4.firebasestorage.app",
-  messagingSenderId: "401798876032",
-  appId: "1:401798876032:web:8965e6381db54eb207cf65"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
+  persistence: getReactNativePersistence(AsyncStorage),
 });
+
 export const db = getFirestore(app);
